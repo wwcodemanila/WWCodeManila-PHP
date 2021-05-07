e13ed6ddafdc4136034712b51f6c3910151023d0
Karen Matala <karenirene.cano1203@gmail.com>
2021-05-07 11:19:41 +0800

    Move counter to a separate route, create new blade for contact form

--- laravel-livewire/routes/web.php
+++ laravel-livewire/routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('contact');
+});
+
+Route::get('/counter', function () {
+    return view('counter');
 });
