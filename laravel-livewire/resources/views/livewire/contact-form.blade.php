1cf1206d2339e49da9f1a74b43ec2ca835395ee3
nressa <mressanova@gmail.com>
2021-05-08 13:31:21 +0800

    Justify center the contact form

--- laravel-livewire/resources/views/livewire/contact-form.blade.php
+++ laravel-livewire/resources/views/livewire/contact-form.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-wrap  content-center ">
+<div class="flex flex-wrap  justify-center m-6">
    <form class="w-full max-w-lg">
    <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
